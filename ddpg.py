6efbb8d6ad9e1ff436dacfec646b1147d14bf23d
david1996a <david1996a@gmail.com>
2021-02-08 00:33:17 +0100

    ddpg

--- ddpg.py
+++ ddpg.py
@@ -10,4 +10,10 @@ class DDPG(object):
 		self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR, weight_decay=WEIGHT_DECAY_actor)
 
 		#Critic network
-		
+		self.critic_local = Critic(num_agents*state_size, num_agents*action_size).to(DEVICE)
+        self.critic_target = Critic(num_agents*state_size, num_agents*action_size).to(DEVICE)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY_critic)
+
+        # Make sure target is initialized with the same weight as the source (makes a big difference)
+        self.hard_update(self.actor_target, self.actor_local)
+        self.hard_update(self.critic_target, self.critic_local)
